bc5c249b45012364246b9d292cef32160e80f773
shuo.han <shuo.han@bianlifeng.com>
2020-03-12 20:07:44 +0800

    update

--- README.md
+++ README.md
@@ -1,14 +1,14 @@
 **工程目录**
-- chineseocr_lite 字符识别
-- data-imageGenerator 数据生成
-- face_recognition 人脸识别
-- facetracker 人脸跟踪
+- [chineseocr_lite 字符识别](https://github.com/hanshuobest/deeplearning/tree/master/chineseocr_lite)
+- [data-imageGenerator 数据生成](https://github.com/hanshuobest/deeplearning/tree/master/data-imageGenerator)
+- [face_recognition 人脸识别](https://github.com/hanshuobest/deeplearning/tree/master/face_recognition)
+- [facetracker 人脸跟踪](https://github.com/hanshuobest/deeplearning/tree/master/facetracker)
 - Faster-RCNN 
 - [GAN](https://github.com/hanshuobest/deeplearning/tree/master/GAN)
 - keras-fire-detection
 - keras-frcnn
-- service-streamer
-- tensorflow
+- [service-streamer](https://github.com/hanshuobest/deeplearning/tree/master/service-streamer)
+
 
 
 
--- service-streamer/example_vision/test.lua
+++ service-streamer/example_vision/test.lua
@@ -1,7 +1,35 @@
-local function writeFile(fileName,content)
-    local f = assert(io.open(fileName,'w'))
-    f:write(content)
-    f:close()
+function read_bin_file(path)
+    local file, errorMessage = io.open(path, "rb")
+    if not file then 
+        error("Could not read the file:" .. errorMessage .. "\n")
+    end
+
+    local content = file:read "*all"
+    file:close()
+    return content
 end
 
-writeFile('test.txt','5.Java')
+local Boundary = "----WebKitFormBoundaryePkpFF7tjBAqx29L"
+local BodyBoundary = "--" .. Boundary
+local LastBoundary = "--" .. Boundary .. "--"
+
+local CRLF = "\r\n"
+
+local Filename = "0166.png"
+local FileBody = read_bin_file(Filename)
+FileBody = 0
+
+local ContentDisposition = "Content-Disposition: form-data; name=\"content\"; filename=\"" .. Filename .. "\""
+print(ContentDisposition)
+local ContentDisposition = "Content-Disposition: form-data; name=\"imageName\""
+print(ContentDisposition)
+
+
+-- wrk.method = "POST"
+-- wrk.headers["Content-Type"] = "multipart/form-data; boundary=" .. Boundary
+-- wrk.body = BodyBoundary .. CRLF .. ContentDisposition .. CRLF .. CRLF .. FileBody .. CRLF .. LastBoundary
+
+-- local headers = "multipart/form-data; boundary=" .. Boundary
+-- print(headers)
+local body = BodyBoundary .. CRLF .. ContentDisposition .. CRLF .. CRLF .. FileBody .. CRLF .. LastBoundary
+print(body)
